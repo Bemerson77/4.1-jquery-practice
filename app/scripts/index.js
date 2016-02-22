fe097a2842c09b823889c47820f8f13d0836638a
Brandon Emerson <Brandon.Emerson77@gmail.com>
2016-02-22 18:22:40 -0500

    end of day

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,12 @@
 console.log("Hello World!");
+var $ = require('jquery');
+
+
+$('.main-links').on('click', function(e){
+  if ($(this).children('.collapseable').hasClass('hide')) {
+    $(this).children('.collapseable').removeClass('hide');
+    $(this).siblings().children('.collapseable').addClass('hide');
+  }else{
+    $(this).children('.collapseable').toggleClass('hide');
+  }
+});
